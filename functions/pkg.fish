e354891c552c1ee0c4a87d623ff828fe85d5989f
Mark Skelton <mskelton@widen.com>
2021-03-05 11:44:26 -0600

    Add better error message when package.json file is not found

--- functions/pkg.fish
+++ functions/pkg.fish
@@ -1,3 +1,10 @@
 function pkg --description 'Parse package.json files'
-  jq ".$argv" package.json
+  if test -f package.json
+    jq ".$argv" package.json
+  else
+    set -l red (set_color --bold red)
+    set -l normal (set_color normal)
+
+    echo -n -s $red "Error: " $normal "Couldn't find a package.json file in the current directory."
+  end
 end
