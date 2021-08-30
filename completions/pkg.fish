4995a4402e737cc8bd12be5f4efecad0a7d5553c
Mark Skelton <mskelton@widen.com>
2021-08-30 16:26:47 -0500

    Update complete to get the available keys from package.json

--- completions/pkg.fish
+++ completions/pkg.fish
@@ -1,1 +1,12 @@
-complete -c pkg --no-files -a "author bin browser bugs bundledDependencies config contributors cpu dependencies description devDependencies directories engines files funding homepage keywords license main man name optionalDependencies os peerDependencies peerDependenciesMeta private publishConfig repository resolutions scripts version workspaces"
+# Get the list of package.json keys in the current directory so only keys that
+# exist are completed.
+function __get_package_json_keys
+  if test -e package.json
+    jq -r 'keys | join("\n")' package.json
+  else
+    echo ""
+  end
+end
+
+# Complete the keys in package.json
+complete -c pkg -f -n "not __fish_seen_subcommand_from (__get_package_json_keys)" -ka '(__get_package_json_keys)'
